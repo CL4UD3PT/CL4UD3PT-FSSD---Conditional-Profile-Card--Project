de7ea487bfa58352304d5664ced501961e0782c2
CL4UD3PT <cifpinho@gmail.com>
2023-03-25 13:39:16 +0000

    linkedin variable

--- src/js/app.js
+++ src/js/app.js
@@ -45,7 +45,9 @@ function render(variables = {}) {
             <li><a href="${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
